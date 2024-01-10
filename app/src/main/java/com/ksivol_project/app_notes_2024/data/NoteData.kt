7dd7a81d0c21c5b71e9f6d060eaae75304ea61e4
Ksivol <surkin-ant@mail.com>
2024-01-10 19:13:33 +0400

    Create NoteData

---
+++ app/src/main/java/com/ksivol_project/app_notes_2024/data/NoteData.kt
@@ -0,0 +1,25 @@
+package com.ksivol_project.app_notes_2024.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.sql.Timestamp
+
+@Entity(tableName = "Notes")
+data class NoteData(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int?,
+
+    @ColumnInfo(name = "dateStart")
+    val dateStart: Timestamp,
+
+    @ColumnInfo(name = "dateFinish")
+    val dateFinish: Timestamp,
+
+    @ColumnInfo(name = "name")
+    val name: String,
+
+    @ColumnInfo(name = "description")
+    val description: String,
+
+    )
