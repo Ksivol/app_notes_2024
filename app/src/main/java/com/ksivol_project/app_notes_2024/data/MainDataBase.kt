57f0255492343710f1401c334ced45e460e0a046
Ksivol <surkin-ant@mail.com>
2024-01-11 14:32:37 +0400

    Create MainDataBase

--- app/src/main/java/com/ksivol_project/app_notes_2024/data/MainDataBase.kt
+++ app/src/main/java/com/ksivol_project/app_notes_2024/data/MainDataBase.kt
@@ -1,9 +1,29 @@
 package com.ksivol_project.app_notes_2024.data
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
- abstract class MainDataBase: RoomDatabase() {
 
+@Database(entities = [NoteData::class], version = 1)
 
+abstract class MainDataBase : RoomDatabase() {
 
+    companion object {
+        @Volatile
+        private var INSTANCE: MainDataBase? = null
+        fun getDataBase(context: Context): MainDataBase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    MainDataBase::class.java,
+                    "notes.db"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+
+    }
 }
