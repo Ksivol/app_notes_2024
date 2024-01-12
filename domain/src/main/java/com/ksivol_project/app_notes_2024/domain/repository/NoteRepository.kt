5f556976805566d8922cb28a33f801f16ce26506
Ksivol <surkin-ant@mail.com>
2024-01-12 16:13:05 +0400

    Create NoteRepository

--- domain/src/main/java/com/ksivol_project/app_notes_2024/domain/repository/NoteRepository.kt
+++ domain/src/main/java/com/ksivol_project/app_notes_2024/domain/repository/NoteRepository.kt
@@ -1,4 +1,16 @@
 package com.ksivol_project.app_notes_2024.domain.repository
 
 interface NoteRepository {
+
+    suspend fun saveNote()
+
+    suspend fun deleteNote()
+
+    suspend fun updateNote()
+
+    suspend fun getNoteByDate()
+
+    suspend fun SearchNoteByName()
+
+
 }
