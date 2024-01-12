691c2d07c40ddf813964f93718a4f883cd5fd348
Ksivol <surkin-ant@mail.com>
2024-01-12 16:23:50 +0400

    Create NoteStorageImpl

--- data/src/main/java/com/ksivol_project/app_notes_2024/data/storage/NoteStorageImpl.kt
+++ data/src/main/java/com/ksivol_project/app_notes_2024/data/storage/NoteStorageImpl.kt
@@ -1,4 +1,29 @@
 package com.ksivol_project.app_notes_2024.data.storage
 
-class NoteStorageImpl {
+import com.ksivol_project.app_notes_2024.data.database.MainDataBase
+
+class NoteStorageImpl(database: MainDataBase) : NoteStorage {
+
+    val dao = database.getDAO()
+
+    override suspend fun saveNote() {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteNote() {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun updateNote() {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getNoteByDate() {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun searchNoteByName() {
+        TODO("Not yet implemented")
+    }
+
 }
