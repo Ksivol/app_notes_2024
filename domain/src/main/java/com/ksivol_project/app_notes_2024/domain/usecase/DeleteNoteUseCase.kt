50d1b264d895bbd332e10561347bfb4a5ba44c8e
Ksivol <surkin-ant@mail.com>
2024-01-13 16:49:40 +0400

    UseCases

--- domain/src/main/java/com/ksivol_project/app_notes_2024/domain/usecase/DeleteNoteUseCase.kt
+++ domain/src/main/java/com/ksivol_project/app_notes_2024/domain/usecase/DeleteNoteUseCase.kt
@@ -1,4 +1,11 @@
 package com.ksivol_project.app_notes_2024.domain.usecase
 
-class DeleteNoteUseCase {
+import com.ksivol_project.app_notes_2024.domain.entities.NoteDomain
+import com.ksivol_project.app_notes_2024.domain.repository.NoteRepository
+
+class DeleteNoteUseCase(private val noteRepository: NoteRepository) {
+
+    suspend fun execute(note: NoteDomain) {
+        note.id?.let { noteRepository.deleteNote(id = it) }
+    }
 }
--- domain/src/main/java/com/ksivol_project/app_notes_2024/domain/usecase/GetNoteByDateUseCase.kt
+++ domain/src/main/java/com/ksivol_project/app_notes_2024/domain/usecase/GetNoteByDateUseCase.kt
@@ -1,4 +1,16 @@
 package com.ksivol_project.app_notes_2024.domain.usecase
 
-class GetNoteByDateUseCase {
+import com.ksivol_project.app_notes_2024.domain.entities.NoteDomain
+import com.ksivol_project.app_notes_2024.domain.repository.NoteRepository
+import kotlinx.coroutines.flow.Flow
+
+
+class GetNoteByDateUseCase(private val noteRepository: NoteRepository) {
+
+    fun execute(note: NoteDomain): Flow<List<NoteDomain>> {
+        return noteRepository.getNoteByDate(
+            dateStart = note.dateStart.toString(),
+            dateFinish = note.dateFinish.toString(),
+        )
+    }
 }
