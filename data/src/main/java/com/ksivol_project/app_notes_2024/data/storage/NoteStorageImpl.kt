fe38e0d254c5a950b2e92e81db082fd6dc4eb442
Ksivol <surkin-ant@mail.com>
2024-01-13 16:49:17 +0400

    Implementation NoteStorageImpl

--- data/src/main/java/com/ksivol_project/app_notes_2024/data/storage/NoteStorageImpl.kt
+++ data/src/main/java/com/ksivol_project/app_notes_2024/data/storage/NoteStorageImpl.kt
@@ -1,26 +1,30 @@
 package com.ksivol_project.app_notes_2024.data.storage
 
 import com.ksivol_project.app_notes_2024.data.database.MainDataBase
+import com.ksivol_project.app_notes_2024.data.entities.NoteData
 
 class NoteStorageImpl(database: MainDataBase) : NoteStorage {
 
     val dao = database.getDAO()
 
-    override suspend fun saveNote() {
-        TODO("Not yet implemented")
+    override suspend fun saveNote(note: NoteData) {
+        dao.saveNote(note = note)
     }
 
-    override suspend fun deleteNote() {
-        TODO("Not yet implemented")
+    override suspend fun deleteNote(id: Int) {
+        dao.deleteNote(id = id)
     }
 
-    override suspend fun updateNote() {
-        TODO("Not yet implemented")
+    override suspend fun updateNote(note: NoteData) {
+        dao.updateNote(note = note)
     }
 
-    override suspend fun getNoteByDate() {
-        TODO("Not yet implemented")
-    }
+    override fun getNoteByDate(dateStart: String, dateFinish: String) =
+        dao.getNoteByDate(
+            dateStart = dateStart,
+            dateFinish = dateFinish
+        )
+
 
     override suspend fun searchNoteByName() {
         TODO("Not yet implemented")
