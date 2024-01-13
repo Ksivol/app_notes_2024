7e6526be299d37538bc998321bbd5acf383b8c80
Ksivol <surkin-ant@mail.com>
2024-01-13 16:48:03 +0400

    getNoteByDate

--- data/src/main/java/com/ksivol_project/app_notes_2024/data/storage/NoteStorage.kt
+++ data/src/main/java/com/ksivol_project/app_notes_2024/data/storage/NoteStorage.kt
@@ -1,14 +1,18 @@
 package com.ksivol_project.app_notes_2024.data.storage
 
+import com.ksivol_project.app_notes_2024.data.entities.NoteData
+import kotlinx.coroutines.flow.Flow
+
 interface NoteStorage {
 
-    suspend fun deleteNote()
+    suspend fun saveNote(note: NoteData)
 
-    suspend fun saveNote()
+    suspend fun deleteNote(id: Int)
 
-    suspend fun updateNote()
+    suspend fun updateNote(note: NoteData)
 
-    suspend fun getNoteByDate()
+    fun getNoteByDate(dateStart: String, dateFinish: String): Flow<List<NoteData>>
 
     suspend fun searchNoteByName()
+
 }
